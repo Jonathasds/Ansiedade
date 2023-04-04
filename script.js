db8e87b9a272ec2ae2df5b3f02dd2d64bc40e814
Jonathas <jonathadelgado@gmail.com>
2023-04-03 22:54:32 -0300

    inicio site ansiedade

---
+++ script.js
@@ -0,0 +1,15 @@
+const openModal = document.querySelector("#open-modal");
+const closeModal = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal")
+const fade = document.querySelector("#fade")
+
+
+const toggleModal = () => {
+  modal.classList.toggle("hide")
+  fade.classList.toggle("hide")
+}
+
+
+[openModal, closeModal, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
